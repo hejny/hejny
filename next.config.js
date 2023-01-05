b7470768eac9fd771876f35f73f530908e24c3a3
Pavol Hejny <me@pavolhejny.com>
2023-01-05 21:14:48 +0100

    exportPathMap

--- next.config.js
+++ next.config.js
@@ -1,24 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    // !!! Remove or keep basePath: 'https://prototyping.hejny.org/' /* <- !!! */,
-
-    // TODO: !!! Go through all option @see https://nextjs.org/docs/api-reference/next.config.js/introduction
-
     images: {
         unoptimized: true /* <- TODO: Optimize images, all images should be in some modern format like webp/svg */,
     },
 
-    /* 
-    TODO: Avoid warn - Statically exporting a Next.js application via `next export` disables API routes.
-
-    async exportPathMap(/* defaultPathMap, { dev, dir, outDir, distDir, buildId } * /) {
+    async exportPathMap() {
         return {
             '/': { page: '/' },
         };
     },
-
-    */
 };
 
 module.exports = nextConfig;
+
+/**
+ * TODO: Solve warning during export:
+ *     > warn  - Statically exporting a Next.js application via `next export` disables API routes.
+ * TODO: Remove or keep basePath: 'https://prototyping.hejny.org/'
+ * TODO: !!! Go through all option @see https://nextjs.org/docs/api-reference/next.config.js/introduction
+ */
