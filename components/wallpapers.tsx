bbfef9e163cb06ae14281a4be97d07463a3d8da1
Pavol Hejny <me@pavolhejny.com>
2023-01-05 23:15:47 +0100

    🖼️  Generate wallpapers library
    🔼 This commit was automatically generated by map scripts

--- components/wallpapers.tsx
+++ components/wallpapers.tsx
@@ -1,15 +1,76 @@
+/**
+ * 🏭 GENERATED WITH 🖼️ Generate wallpapers library
+ * ⚠️ Warning: Do not edit by hand, all changes will be lost on next execution!
+ */
+
 import Image from 'next/image';
-import cave_of_ideas_in_lightbulb from '../public/wallpapers/Pavol_Hejn_cave_of_ideas_in_lightbulb_with_transparent_look_thr_d3273ad0-097f-4011-b799-1c379bb05ee3.png';
-import cave_of_ideas_image from '../public/wallpapers/Pavol_Hejn_cave_of_ideas_with_transparent_look_through_fe3480c5-76af-45da-ac4e-5177062bcb6b.png';
+import caveOfIdeasInLightbulbWithTransparentLookThr from './../../public/wallpapers/Pavol_Hejn_cave_of_ideas_in_lightbulb_with_transparent_look_thr_d3273ad0-097f-4011-b799-1c379bb05ee3.png';
+import caveOfIdeasWithTransparentLookThrough from './../../public/wallpapers/Pavol_Hejn_cave_of_ideas_with_transparent_look_through_285b9893-443d-47e2-80d4-763b73ae4a44.png';
+import caveOfIdeasWithTransparentLookThrough2 from './../../public/wallpapers/Pavol_Hejn_cave_of_ideas_with_transparent_look_through_fe3480c5-76af-45da-ac4e-5177062bcb6b.png';
+import caveWithPrototypingLaboratory from './../../public/wallpapers/Pavol_Hejn_cave_with_prototyping_laboratory_ca9b82b9-0ded-44a3-b7ec-344ebb539439.png';
+import caveWithPrototypingLaboratory2 from './../../public/wallpapers/Pavol_Hejn_cave_with_prototyping_laboratory_e224a8e2-8e46-405e-9182-f9d31c39979e.png';
+import lightbulbOfIdeasWithTransparentLookThrough from './../../public/wallpapers/Pavol_Hejn_lightbulb_of_ideas_with_transparent_look_through_1df5cc46-efcf-4014-929f-3e55c2542d50.png';
 
-export function CaveOfIdeasImage() {
-    return <Image alt="!!!" src={cave_of_ideas_image} />;
-}
-
-export function CaveOfIdeasInLightbulbImage() {
-    return <Image alt="!!!" src={cave_of_ideas_in_lightbulb} />;
+/**
+ * Image of cave of ideas in lightbulb with transparent look thr
+ *
+ * @see https://www.midjourney.com/app/jobs/d3273ad0-097f-4011-b799-1c379bb05ee3.png
+ */
+export function CaveOfIdeasInLightbulbWithTransparentLookThrImage() {
+    return (
+        <Image
+            alt="cave of ideas in lightbulb with transparent look thr"
+            src={caveOfIdeasInLightbulbWithTransparentLookThr}
+        />
+    );
 }
 
 /**
- * TODO: Generate with script
+ * Image of cave of ideas with transparent look through
+ *
+ * @see https://www.midjourney.com/app/jobs/285b9893-443d-47e2-80d4-763b73ae4a44.png
  */
+export function CaveOfIdeasWithTransparentLookThroughImage() {
+    return <Image alt="cave of ideas with transparent look through" src={caveOfIdeasWithTransparentLookThrough} />;
+}
+
+/**
+ * Image of cave of ideas with transparent look through
+ *
+ * @see https://www.midjourney.com/app/jobs/fe3480c5-76af-45da-ac4e-5177062bcb6b.png
+ */
+export function CaveOfIdeasWithTransparentLookThroughImage2() {
+    return <Image alt="cave of ideas with transparent look through" src={caveOfIdeasWithTransparentLookThrough2} />;
+}
+
+/**
+ * Image of cave with prototyping laboratory
+ *
+ * @see https://www.midjourney.com/app/jobs/ca9b82b9-0ded-44a3-b7ec-344ebb539439.png
+ */
+export function CaveWithPrototypingLaboratoryImage() {
+    return <Image alt="cave with prototyping laboratory" src={caveWithPrototypingLaboratory} />;
+}
+
+/**
+ * Image of cave with prototyping laboratory
+ *
+ * @see https://www.midjourney.com/app/jobs/e224a8e2-8e46-405e-9182-f9d31c39979e.png
+ */
+export function CaveWithPrototypingLaboratoryImage2() {
+    return <Image alt="cave with prototyping laboratory" src={caveWithPrototypingLaboratory2} />;
+}
+
+/**
+ * Image of lightbulb of ideas with transparent look through
+ *
+ * @see https://www.midjourney.com/app/jobs/1df5cc46-efcf-4014-929f-3e55c2542d50.png
+ */
+export function LightbulbOfIdeasWithTransparentLookThroughImage() {
+    return (
+        <Image
+            alt="lightbulb of ideas with transparent look through"
+            src={lightbulbOfIdeasWithTransparentLookThrough}
+        />
+    );
+}
