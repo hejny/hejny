30c88403b55b65ab5d2f941f64b7ff413a90f834
Pavol Hejny <me@pavolhejny.com>
2023-01-05 20:33:59 +0100

    Adding exportPathMap

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
     // !!! Remove or keep basePath: 'https://prototyping.hejny.org/' /* <- !!! */,
 
     // TODO: !!! Go through all option @see https://nextjs.org/docs/api-reference/next.config.js/introduction
+
+    async exportPathMap(/* defaultPathMap, { dev, dir, outDir, distDir, buildId } */) {
+        return {
+            '/': { page: '/' },
+        };
+    },
 };
 
 module.exports = nextConfig;
