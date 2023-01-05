93dcdc12f832227e933f022c1d8ac5f57c283cda
Pavol Hejny <me@pavolhejny.com>
2023-01-05 21:34:04 +0100

    Better split of services

--- components/Services/Service.tsx
+++ components/Services/Service.tsx
@@ -1,35 +1,11 @@
-import Image from 'next/image';
-import caveOfIdeasImage from '../../public/wallpapers/Pavol_Hejn_cave_of_ideas_with_transparent_look_through_fe3480c5-76af-45da-ac4e-5177062bcb6b.png';
+import { PropsWithChildren } from 'react';
+import styles from './Service.module.css';
 
-export function Service({ title }: { title: string }) {
+export function Service({ title, children }: PropsWithChildren<{ title: string }>) {
     return (
-        <div
-            style={{
-                display: 'grid',
-                gridTemplateColumns: `1fr`,
-                gridTemplateRows: `5fr 1fr`,
-                alignItems: 'stretch',
-                justifyItems: 'stretch',
-            }}
-        >
-            <div
-                style={{
-                    outline: '1px dotted red',
-                    gridColumn: `1 / span 1`,
-                    gridRow: `1 / span 2`,
-                }}
-            >
-                <Image alt="!!!" src={caveOfIdeasImage} style={{ width: '100%', height: 'auto' }} />
-            </div>
-            <div
-                style={{
-                    outline: '1px dotted red',
-                    gridColumn: `1 / span 1`,
-                    gridRow: `2 / span 1`,
-                }}
-            >
-                {title}
-            </div>
+        <div className={styles.service}>
+            <div className={styles['image-container']}>{children}</div>
+            <div className={styles.title}>{title}</div>
         </div>
     );
 }
--- components/Services/Services.tsx
+++ components/Services/Services.tsx
@@ -1,12 +1,21 @@
+import Image from 'next/image';
+import cave_of_ideas_in_lightbulb from '../../public/wallpapers/Pavol_Hejn_cave_of_ideas_in_lightbulb_with_transparent_look_thr_d3273ad0-097f-4011-b799-1c379bb05ee3.png';
 import { Service } from './Service';
 import styles from './Services.module.css';
 
+// !!! Wallpapers image library + script
 export function Services() {
     return (
         <div className={styles.services}>
-            <Service title="Hello1" />
-            <Service title="Hello2" />
-            <Service title="Hello3" />
+            <Service title="Hello1">
+                <Image alt="!!!" src={cave_of_ideas_in_lightbulb}  />
+            </Service>
+            <Service title="Hello2">
+                <Image alt="!!!" src={cave_of_ideas_in_lightbulb}/>
+            </Service>
+            <Service title="Hello3">
+                <Image alt="!!!" src={cave_of_ideas_in_lightbulb} />
+            </Service>
         </div>
     );
 }
