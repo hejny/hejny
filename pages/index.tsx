1f70c0e97f26939c39fa341c58afa81117a2fad0
Pavol Hejny <me@pavolhejny.com>
2023-01-08 13:17:57 +0100

    Extract styles of intex into css module

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { Inter } from '@next/font/google';
 import Head from 'next/head';
 import { Cave } from '../components/Cave/Cave';
 import { Services } from '../components/Services/Services';
+import styles from './index.module.css';
 
 // !!! What is this - inter/Inter
 const inter = Inter({ subsets: ['latin'] });
@@ -19,63 +20,17 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <div
-                style={{
-                    width: `100%`,
-
-                    display: 'grid',
-
-                    // TODO: !!! On mobile strech cave from edge to edge
-                    gridTemplateColumns: `2vw 1fr 3fr 1fr 2vw`,
-                    gridTemplateRows: `0px auto 20vh minmax`,
-
-                    alignItems: 'stretch',
-                    justifyItems: 'stretch',
-                }}
-            >
-                <div
-                    style={{
-                        zIndex: 1,
-                        // outline: '1px dotted red',
-
-                        gridColumn: `2 / span 3`,
-                        gridRow: `2 / span 1`,
-                    }}
-                >
+            <div className={styles.page}>
+                <div className={styles.cave}>
                     <Cave />
                 </div>
 
-                <div
-                    style={{
-                        zIndex: 2,
-                        // outline: '1px dotted red',
-                        gridColumn: `2 / span 3`,
-                        gridRow: `3 / span 2`,
-                        textAlign: 'center',
-                        color: 'white',
-                        textShadow: `#ff5500 0px 0px 10px`,
-                    }}
-                >
+                <div className={styles.content}>
                     {/* TODO: Make as <Component/> */}
-                    <h1
-                        style={{
-                            fontFamily: '"PassionsConflict", Helvetica Neue',
-                            fontSize: '60px',
-
-                            // TODO: DRY
-                            margin: 0,
-                            padding: 0,
-                            marginBlockStart: 0,
-                            marginBlockEnd: 0,
-                            marginInlineStart: 0,
-                            marginInlineEnd: 0,
-                        }}
-                    >
-                        Wizard
-                    </h1>
+                    <h1>Wizard</h1>
 
                     {/* TODO: Make as <Component/> */}
-                    <p style={{ width: 'calc(50% + 1vw)', margin: 'auto' }}>
+                    <p>
                         AI, Neural networks, VR/AR, WebGL, WebVR, Webassembly, WebSockets. Bitcoin, Ethereum, Cardano,
                         Crypto, Smart contracts, Web3, ...
                     </p>
