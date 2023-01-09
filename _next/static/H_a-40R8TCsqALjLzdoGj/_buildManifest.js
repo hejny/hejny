37c4d2d656de803e2c68718864a90eaee8cb73fd
hejny <me@pavolhejny.com>
2023-01-09 09:34:55 +0000

    Updates

---
+++ _next/static/H_a-40R8TCsqALjLzdoGj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0908dd6340fef2b0.css","static/chunks/pages/index-9b373213e6cde8d6.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
