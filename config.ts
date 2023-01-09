986e4f0b2236c3737b7bf7798fadb4b4bb53a7e4
Pavol Hejny <me@pavolhejny.com>
2023-01-09 14:26:03 +0100

    Configuration

---
+++ config.ts
@@ -0,0 +1,3 @@
+export const DEBUG = {
+    showGrid: true,
+};
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,23 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import { createContext } from 'react';
+import { DEBUG } from '../config';
+
+export const DebugContext = createContext<typeof DEBUG>(DEBUG);
 
 export default function Document() {
     return (
         <Html lang="en">
             <Head />
             <body>
-                <Main />
+                <DebugContext.Provider value={DEBUG}>
+                    <Main />
+                </DebugContext.Provider>
                 <NextScript />
             </body>
         </Html>
     );
 }
+
+/**
+ * TODO: Is this a good place to insert a <DebugContext.Provider
+ */
