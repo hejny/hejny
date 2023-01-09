0cb71772a4702e2347cee8cd9ef20084dda290b6
Pavol Hejny <me@pavolhejny.com>
2023-01-09 15:58:54 +0100

    ­čž╣ Prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/reset.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-
 export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
 }
