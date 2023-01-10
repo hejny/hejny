8a80e1518062f14e8c8c609d0ed1233b18827c65
hejny <me@pavolhejny.com>
2023-01-10 09:27:14 +0000

    Updates

---
+++ wallpapers/index.tsx
@@ -0,0 +1,100 @@
+/**
+ * 🏭 GENERATED WITH 🖼️ Generate wallpapers library
+ * ⚠️ Warning: Do not edit by hand, all changes will be lost on next execution!
+ */
+
+import Image from 'next/image';
+import cave_of_ideas_in_lightbulb_with_transparent_look_thr from './Pavol_Hejn_cave_of_ideas_in_lightbulb_with_transparent_look_thr_d3273ad0-097f-4011-b799-1c379bb05ee3.png';
+import cave_of_ideas_with_transparent_look_through from './Pavol_Hejn_cave_of_ideas_with_transparent_look_through_285b9893-443d-47e2-80d4-763b73ae4a44.png';
+import cave_of_ideas_with_transparent_look_through2 from './Pavol_Hejn_cave_of_ideas_with_transparent_look_through_fe3480c5-76af-45da-ac4e-5177062bcb6b.png';
+import cave_with_presentation from './Pavol_Hejn_cave_with_presentation_dec31ff2-3b9d-42b9-b84f-f87d02d6a391.png';
+import cave_with_prototyping_laboratory from './Pavol_Hejn_cave_with_prototyping_laboratory_ca9b82b9-0ded-44a3-b7ec-344ebb539439.png';
+import cave_with_prototyping_laboratory2 from './Pavol_Hejn_cave_with_prototyping_laboratory_e224a8e2-8e46-405e-9182-f9d31c39979e.png';
+import lightbulb_of_ideas_with_transparent_look_through from './Pavol_Hejn_lightbulb_of_ideas_with_transparent_look_through_1df5cc46-efcf-4014-929f-3e55c2542d50.png';
+
+/**
+ * Image of cave of ideas in lightbulb with transparent look thr
+ *
+ * @see https://www.midjourney.com/app/jobs/d3273ad0-097f-4011-b799-1c379bb05ee3
+ */
+export function CaveOfIdeasInLightbulbWithTransparentLookThrImage() {
+    return (
+        <Image
+            alt="cave of ideas in lightbulb with transparent look thr"
+            src={cave_of_ideas_in_lightbulb_with_transparent_look_thr}
+            draggable="false"
+        />
+    );
+}
+
+/**
+ * Image of cave of ideas with transparent look through
+ *
+ * @see https://www.midjourney.com/app/jobs/285b9893-443d-47e2-80d4-763b73ae4a44
+ */
+export function CaveOfIdeasWithTransparentLookThroughImage() {
+    return (
+        <Image
+            alt="cave of ideas with transparent look through"
+            src={cave_of_ideas_with_transparent_look_through}
+            draggable="false"
+        />
+    );
+}
+
+/**
+ * Image of cave of ideas with transparent look through
+ *
+ * @see https://www.midjourney.com/app/jobs/fe3480c5-76af-45da-ac4e-5177062bcb6b
+ */
+export function CaveOfIdeasWithTransparentLookThroughImage2() {
+    return (
+        <Image
+            alt="cave of ideas with transparent look through"
+            src={cave_of_ideas_with_transparent_look_through2}
+            draggable="false"
+        />
+    );
+}
+
+/**
+ * Image of cave with presentation
+ *
+ * @see https://www.midjourney.com/app/jobs/dec31ff2-3b9d-42b9-b84f-f87d02d6a391
+ */
+export function CaveWithPresentationImage() {
+    return <Image alt="cave with presentation" src={cave_with_presentation} draggable="false" />;
+}
+
+/**
+ * Image of cave with prototyping laboratory
+ *
+ * @see https://www.midjourney.com/app/jobs/ca9b82b9-0ded-44a3-b7ec-344ebb539439
+ */
+export function CaveWithPrototypingLaboratoryImage() {
+    return <Image alt="cave with prototyping laboratory" src={cave_with_prototyping_laboratory} draggable="false" />;
+}
+
+/**
+ * Image of cave with prototyping laboratory
+ *
+ * @see https://www.midjourney.com/app/jobs/e224a8e2-8e46-405e-9182-f9d31c39979e
+ */
+export function CaveWithPrototypingLaboratoryImage2() {
+    return <Image alt="cave with prototyping laboratory" src={cave_with_prototyping_laboratory2} draggable="false" />;
+}
+
+/**
+ * Image of lightbulb of ideas with transparent look through
+ *
+ * @see https://www.midjourney.com/app/jobs/1df5cc46-efcf-4014-929f-3e55c2542d50
+ */
+export function LightbulbOfIdeasWithTransparentLookThroughImage() {
+    return (
+        <Image
+            alt="lightbulb of ideas with transparent look through"
+            src={lightbulb_of_ideas_with_transparent_look_through}
+            draggable="false"
+        />
+    );
+}
