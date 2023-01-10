baad8382ee3cd11ef71672f475cbb55649febe20
Pavol Hejny <me@pavolhejny.com>
2023-01-10 10:05:53 +0100

    Add todos

--- components/Items/Items.tsx
+++ components/Items/Items.tsx
@@ -1,6 +1,14 @@
-import { PropsWithChildren } from 'react';
+import { ReactFragment } from 'react';
 import styles from './Items.module.css';
 
-export function Items({ children }: PropsWithChildren<{}>) {
+interface ItemsProps {
+    children: ReactFragment;
+}
+
+export function Items({ children }: ItemsProps) {
     return <div className={styles.items}>{children}</div>;
 }
+
+/**
+ * TODO: !!! Hover of links
+ */
