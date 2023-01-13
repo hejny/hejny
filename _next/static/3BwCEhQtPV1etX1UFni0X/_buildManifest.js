46184061c68f70ae33ba0f67d1943a1ab8ecf6b1
hejny <me@pavolhejny.com>
2023-01-13 20:28:26 +0000

    Updates

--- _next/static/3BwCEhQtPV1etX1UFni0X/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/156-7bb7d8e752526bbc.js","static/css/99adf04d86f4baba.css","static/chunks/pages/index-51c175941516ae75.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
