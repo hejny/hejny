d10dc8be13d34d9734c96d2368c362c141594284
Pavol Hejny <me@pavolhejny.com>
2023-02-20 22:37:03 +0100

    theme-color

--- src/sections/HeadSection/HeadSection.tsx
+++ src/sections/HeadSection/HeadSection.tsx
@@ -19,11 +19,15 @@ export function HeadSection(props: HeadSectionProps) {
 
             <meta property="og:title" content={`From 0 to 1`} />
             <meta property="og:image" content={cave_of_ideas_with_transparent_look_through.src} />
+
+            <meta name="theme-color" content="#000000" />
         </>
     );
 }
 
 /**
+ * TODO: !!! Warning: A title element received an array with more than 1 element as children. In browsers title Elements can only have Text Nodes as children. If the children being rendered output more than a single text node in aggregate the browser will display markup 
+and comments as text in the title and hydration will likely fail and fall back to client rendering
  * TODO: Create better summary
  * TODO: Maybe import from some JSON
  * TODO: What is ideal viewport value
