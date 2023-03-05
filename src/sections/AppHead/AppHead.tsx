1717990044c873f9897992b0cbeb9eaa7c8101d7
Pavol Hejny <me@pavolhejny.com>
2023-03-05 18:17:29 +0100

    Fix title element

--- src/sections/AppHead/AppHead.tsx
+++ src/sections/AppHead/AppHead.tsx
@@ -11,7 +11,7 @@ export function AppHead(props: AppHeadProps) {
 
     return (
         <Head>
-            <title>🧙‍♂️ {!subtitle ? `` : `${subtitle} | `}From 0 to 1</title>
+            <title>{`${!subtitle ? `` : `${subtitle} ✨ `}From 0 to 1`}</title>
             <meta
                 name="description"
                 content="With the ever-evolving landscape of technology, it can be challenging to keep up to date and use it to its potential in your business. However, by incorporating cutting-edge tools such as advanced browser APIs, >Web Assembly, using TypeScript, and benefiting from GPT when writing code, you can code, you can differentiate your business and achieve new levels of efficiency, innovation and customer engagement. 🚀"
@@ -28,8 +28,6 @@ export function AppHead(props: AppHeadProps) {
 }
 
 /**
- * TODO: !!! Warning: A title element received an array with more than 1 element as children. In browsers title Elements can only have Text Nodes as children. If the children being rendered output more than a single text node in aggregate the browser will display markup 
-and comments as text in the title and hydration will likely fail and fall back to client rendering
  * TODO: Create better summary
  * TODO: Maybe import from some JSON
  * TODO: What is ideal viewport value
