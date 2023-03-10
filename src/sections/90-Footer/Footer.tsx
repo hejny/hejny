3d04ae3413e3d58828b0a36ab767bb061973cd1a
Pavol Hejny <me@pavolhejny.com>
2023-03-10 16:00:43 +0100

    Change nonce

--- src/sections/90-Footer/Footer.tsx
+++ src/sections/90-Footer/Footer.tsx
@@ -35,7 +35,7 @@ export function FooterSection() {
                 </li>
 
                 <li>
-                    <Link href="/!!!">1</Link>
+                    <Link href="/!!!">2</Link>
                 </li>
 
                 {/*
