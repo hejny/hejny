a83c21f95aeef3b1e1b963fec14721feb12d7c2a
Restyled.io <commits@restyled.io>
2023-03-10 01:17:44 +0000

    Restyled by prettier

--- next.config.js
+++ next.config.js
@@ -2,29 +2,29 @@ const withExportImages = require('next-export-optimize-images');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode : true,
-  experimental : {
-    appDir : true,
-  },
-  images : {
-    remotePatterns : [
-      {
-        protocol : 'https',
-        hostname : 'www.gravatar.com',
-        port : '',
-        pathname : '/avatar/**',
-      },
-    ],
-  },
+    reactStrictMode: true,
+    experimental: {
+        appDir: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'www.gravatar.com',
+                port: '',
+                pathname: '/avatar/**',
+            },
+        ],
+    },
 
-  async exportPathMap() {
-    return {
-      '/' : {page : '/'},
-      '/about' : {page : '/about'},
-      '/gallery' : {page : '/gallery'},
-      '/contact' : {page : '/contact'},
-    };
-  },
+    async exportPathMap() {
+        return {
+            '/': { page: '/' },
+            '/about': { page: '/about' },
+            '/gallery': { page: '/gallery' },
+            '/contact': { page: '/contact' },
+        };
+    },
 };
 
 module.exports = withExportImages(nextConfig);
