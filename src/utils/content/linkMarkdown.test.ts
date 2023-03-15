ec901468c85f6e0ebb24bdec3e18348459ec91b7
Pavol Hejny <me@pavolhejny.com>
2023-03-15 21:46:43 +0100

    More test cases for linkMarkdown

--- src/utils/content/linkMarkdown.test.ts
+++ src/utils/content/linkMarkdown.test.ts
@@ -15,4 +15,25 @@ describe('linkMarkdown', () => {
             `look at [H-edu](https://h-edu.cz) and [Collboard](https://collboard.com)`,
         );
     });
+
+    it(`add multiple links`, () => {
+        expect(linkMarkdown(`look at H-edu and Collboard`)).toBe(
+            `look at [H-edu](https://h-edu.cz) and [Collboard](https://collboard.com)`,
+        );
+    });
+
+    it(`works with inflection`, () => {
+        expect(linkMarkdown(`Česká společnost ornitologická bez České společnosti ornitologické`)).toBe(
+            `[Česká společnost ornitologická](https://www.birdlife.cz/) bez [České společnosti ornitologické](https://www.birdlife.cz/)`,
+        );
+        expect(linkMarkdown(`Startup Weekend bez Startup Weekendu`)).toBe(
+            `[Startup Weekend](https://startovani.cz/) bez [Startup Weekendu](https://startovani.cz/)`,
+        );
+    });
+
+    it(`will activate acronyms`, () => {
+        expect(linkMarkdown(`VR/AR`)).toBe(`[VR/AR](/technologies/vr-ar)`);
+        expect(linkMarkdown(`AI`)).toBe(`[AI](/technologies/ai)`);
+        expect(linkMarkdown(`Blockchain`)).toBe(`[Blockchain](/technologies/blockchain)`);
+    });
 });
