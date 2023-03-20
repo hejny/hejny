be10f0849e772e149f959ca1965605e3006d888a
Pavol Hejny <me@pavolhejny.com>
2023-03-20 19:32:14 +0100

    Try to fix cs locale version

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,6 +4,7 @@ module.exports = {
     i18n: {
         defaultLocale: 'en',
         locales: ['en', 'cs'],
+        localeDetection: false,
         domains: [
             {
                 domain: 'www.pavolhejny.com',
