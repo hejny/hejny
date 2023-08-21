92d81410f3e8ccdc63a21676653faa415b005d39
Pavol Hejny <me@pavolhejny.com>
2023-08-21 16:28:46 +0200

    Transfer <Items> to pavolhejny

--- src/components/Items/Item.tsx
+++ src/components/Items/Item.tsx
@@ -1,15 +1,24 @@
 import { ReactNode } from 'react';
 import styles from './Item.module.css';
 
+/**
+ * <Item/> used in <Items/>
+ */
 export function Item({ children }: { children: ReactNode }) {
     return <div className={styles.item}>{children}</div>;
 }
 
+/**
+ * <Title/> used in <Item/>
+ */
 Item.Title = function Title({ children }: { children: ReactNode }) {
     // TODO: Probbably place here semantic <h3> tag
     return <div className={styles.title}>{children}</div>;
 };
 
+/**
+ * <FloatingTitle/> used in <Item/>
+ */
 Item.FloatingTitle = function FloatingTitle({ children }: { children: ReactNode }) {
     // TODO: Probbably place here semantic <h3> tag
     return (
@@ -19,10 +28,16 @@ Item.FloatingTitle = function FloatingTitle({ children }: { children: ReactNode 
     );
 };
 
+/**
+ * <Description/> used in <Item/>
+ */
 Item.Description = function Description({ children }: { children: ReactNode }) {
     return <div className={styles.description}>{children}</div>;
 };
 
+/**
+ * <Image/> used in <Item/>
+ */
 Item.Image = function Image({ children }: { children: ReactNode }) {
     return (
         <>
@@ -32,6 +47,9 @@ Item.Image = function Image({ children }: { children: ReactNode }) {
     );
 };
 
+/**
+ * <PersonImage/> used in <Item/>
+ */
 Item.PersonImage = function PersonImage({ children }: { children: ReactNode }) {
     return (
         <>
--- src/components/Items/Items.tsx
+++ src/components/Items/Items.tsx
@@ -2,12 +2,31 @@ import { ReactNode } from 'react';
 import { classNames } from '../../utils/classNames';
 import styles from './Items.module.css';
 
+/**
+ * Renders a div container for the child components
+ *
+ * @param {ItemsProps} props - The props for the Items component.
+ * @returns {JSX.Element} The Items component.
+ */
 interface ItemsProps {
     itemsOnRow?: 2 | 3;
+
+    /**
+     * The items to render
+     *
+     * Note: Use the <Item/> component to render the items
+     */
     children: ReactNode;
 }
 
+/**
+ * Renders a div container for N items
+ */
 export function Items(props: ItemsProps) {
     const { itemsOnRow = 3, children } = props;
     return <div className={classNames(styles.items, styles[`row-with-${itemsOnRow}-items`])}>{children}</div>;
 }
+
+/**
+ * TODO: ItemsProps should contain preferedItemWidth: number
+ */
