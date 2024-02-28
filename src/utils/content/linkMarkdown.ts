d6f1fb5ce1675c69f3869e7d6083bf92137673a4
Pavol Hejny <me@pavolhejny.com>
2024-02-28 20:41:36 +0100

    Autolinks together with WebGPT

--- src/utils/content/linkMarkdown.ts
+++ src/utils/content/linkMarkdown.ts
@@ -1,10 +1,18 @@
 const LINKS = {
+    WebGPT: 'https://webgpt.cz/?partner=ph&utm_medium=referral&utm_source=personal-page&utm_content=pavolhejny.com&utm_campaign=partner-ph',
+    'Ai*nautes': 'https://www.ainautes.com',
+    Ainautes: 'https://www.ainautes.com',
+    Promptbook: 'https://github.com/webgptorg/promptbook',
+    'Prompt book': 'https://github.com/webgptorg/promptbook',
     Collboard: 'https://collboard.com',
     'H-edu': 'https://h-edu.cz',
     'Czech.events': 'https://czech.events',
     'Česká společnost ornitologická': 'https://www.birdlife.cz/',
     'České společnosti ornitologické': 'https://www.birdlife.cz/',
     'Startup Weekendu': 'https://startovani.cz/',
+    'my Githubu': 'https://github.com/hejny',
+    'mém Githubu': 'https://github.com/hejny',
+
     // TODO: !!! 'Startup Weekend': 'https://startovani.cz/',
 
     // TODO: !!! České společnosti ornitologické vs Česká společnost ornitologická
